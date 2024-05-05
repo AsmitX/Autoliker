4115692f7b4c62f77c047b4853d2598ee8e8ab76
AsmitX <162042032+AsmitX@users.noreply.github.com>
2024-05-05 19:14:09 +0545

    Update autoliker.py

--- autoliker.py
+++ autoliker.py
@@ -73,15 +73,13 @@ def show_program():
                     %sINFORMATION%s
  ------------------------------------------------------
 
-    Author     Debby Anggraini 'CiKu370'
-    Name       OSIF 'Open Source Information Facebook'
+    Author     ASMITX'BENZA'
+    Name       ASMITX 'Open Source Information Facebook'
     CodeName   D3b2y
     version    full version
     Date       16/05/2018 09:35:12
-    Team       Blackhole Security
-    Email      xnver404@gmail.com
-    Telegram   @CiKu370
-
+    Email      Asmitadk2@gmail.com
+    Telegram   @Asmit Adk
 * if you find any errors or problems , please contact
   author
 '''%(G,W)
